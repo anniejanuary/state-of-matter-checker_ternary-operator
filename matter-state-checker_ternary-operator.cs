fca75eabbb0de3669cffe999a0eed57fc0fb0883
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-21 17:32:01 +0200

    Update matter-state-checker_ternary-operator.cs

--- matter-state-checker_ternary-operator.cs
+++ matter-state-checker_ternary-operator.cs
@@ -12,20 +12,24 @@ namespace EnhancedIfStatements_TernaryOperators
             // 1 Basic if else statement 
             if (temperature < 0)
                 stateOfMatter = "solid"; //what happened with the curly brackets? dont need them ever?
-            else
+            else if (temperature > 0 && temperature < 100)
                 stateOfMatter = "liquid";
+            else
+                stateOfMatter = "gas";
 
-            Console.WriteLine($"State of matter is {stateOfMatter}"); 
-            
+            Console.WriteLine($"State of matter is {stateOfMatter}");
+
+            temperature += 30;
+
             // 2 Enhanced if statement = in short
             //stateofmatter = temperature < 0 "if yes, then" "solid" "if no then" "liquid")
-            stateOfMatter = temperature < 0 ? "solid" : "liquid";
+            stateOfMatter = temperature < 0 ? "solid" : temperature > 100 ? "gas" : "liquid";
 
             Console.WriteLine($"State of matter is {stateOfMatter}");
 
             //increasing temp after that console write
-            temperature += 30;
-            stateOfMatter = temperature < 0 ? "solid" : "liquid";
+            temperature += 200;
+            stateOfMatter = temperature < 0 ? "solid" : temperature > 100 ? "gas" : "liquid";
             Console.WriteLine($"State of matter is {stateOfMatter}");
 
             Console.ReadKey();
